ca0134ff047ce6f17d01a7abec1c6fdd47dc817d
bradc <brad.cocar@gmail.com>
2024-12-29 11:11:27 -0800

    ChatServer listens on ServerSocket

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,2 +1,17 @@
+import java.net.ServerSocket;
+
 public class ChatServer {
+    private int port;
+
+    public ChatServer(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatServer cs = new ChatServer(4433);
+    }
 }
