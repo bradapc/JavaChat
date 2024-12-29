a9772a5a31b2f6feac70f61b6295adc90cca45a0
bradc <brad.cocar@gmail.com>
2024-12-29 11:46:45 -0800

    ChatClient now connects to localhost.

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,2 +1,17 @@
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class ChatClient {
+    public ChatClient() {
+        try {
+            Socket conn = new Socket(InetAddress.getLocalHost(), 4433);
+            System.out.println("Connected.");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatClient chatClient = new ChatClient();
+    }
 }
