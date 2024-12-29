a1e5595960dab2f1e006d7b89844fba7e7ca3ee3
bradc <brad.cocar@gmail.com>
2024-12-29 14:40:37 -0800

    Added logging

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,7 +1,6 @@
 import java.io.DataOutputStream;
-import java.net.InetSocketAddress;
+import java.io.FileOutputStream;
 import java.net.Socket;
-import java.util.Arrays;
 
 public class ChatServer {
     private SocketThread[] socketThreads;
@@ -25,9 +24,24 @@ public class ChatServer {
         }
     }
 
+    public void logMessage(String logMsg) {
+        try {
+            FileOutputStream fos = new FileOutputStream("log.txt", true);
+            char[] chars = logMsg.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                fos.write(chars[i]);
+            }
+            fos.write('\n');
+            fos.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
     public void receiveMessage(String message, SocketThread socketThread) {
         String username = socketThread.getUsername();
         String newMessage = username + ": " + message + '\0';
+        logMessage(username + ": " + message);
         for (SocketThread sThread : socketThreads) {
             if (sThread != null && !sThread.equals(socketThread)) {
                 try {
